500a0044fd0266f330dd7d685d99b204a2342a31
Jackson Fellows <jtfellows@icloud.com>
2023-09-21 13:38:28 -0400

    Add binary operators.
    Also change some other stuff.

--- evolve.py
+++ evolve.py
@@ -24,6 +24,7 @@ EvolutionParameters = namedtuple(
         "mutation_rate",
         "max_depth",
         "n_generations",
+        "terminal_proba",
     ),
 )
 
@@ -45,24 +46,26 @@ class Evolver:
     def simplify_feature(self, feature):
         def rec(tree):
             match tree:
-                case (f, x):
-                    simplified, rank = rec(x)
-                    if rank + self.operators[f].delta_rank < 1:
-                        return simplified, rank
-                    return (f, simplified), rank + self.operators[f].delta_rank
+                case (f, *x):
+                    simplifieds, ranks = zip(*map(rec, x))
+                    max_rank = max(ranks)
+                    if max_rank + self.operators[f].delta_rank < 1:
+                        # No-op.
+                        return simplifieds[0], max_rank
+                    return (f, *simplifieds), max_rank + self.operators[f].delta_rank
                 case x:
                     return x, len(self.terminals[x].shape)
 
         return rec(feature)[0]
 
-    @lru_cache(maxsize=256)  # TODO: This is not the optimal caching strategy.
+    @lru_cache(maxsize=128)  # TODO: This is not the optimal caching strategy.
     def eval_feature_rec(self, feature):
         match feature:
-            case (f, x):
-                x_ = self.eval_feature_rec(x)
-                if len(x_.shape) + self.operators[f].delta_rank < 1:
-                    return x_
-                return np.nan_to_num(self.operators[f].apply(x_))
+            case (f, *x):
+                x_ = list(map(self.eval_feature_rec, x))
+                if min(len(x__.shape) for x__ in x_) + self.operators[f].delta_rank < 1:
+                    return x_[0]
+                return np.nan_to_num(self.operators[f].apply(*x_))
             case x:
                 return self.terminals[x]
 
@@ -101,23 +104,33 @@ class Evolver:
         )  # absolute correlation [0,1]
         return np.clip(score - 0.17 * simil, 0, None)
 
-    def random_feature(self, max_depth):
-        if max_depth == 0 or random.random() < 0.3:
+    def random_feature(self, max_depth, terminal_proba):
+        if max_depth == 0 or random.random() < terminal_proba:
             return random.choice(list(self.terminals.keys()))
+        head = random.choice(list(self.operators.keys()))
         return (
-            random.choice(list(self.operators.keys())),
-            self.random_feature(max_depth - 1),
+            head,
+            *[
+                self.random_feature(max_depth - 1, terminal_proba)
+                for _ in range(self.operators[head].arity)
+            ],
         )
 
     def point_mutate_feature(self, feature, p=0.3):
         match feature:
-            case (f, x):
+            case (f, *x):
                 if random.random() < p:
                     return (
-                        random.choice(list(self.operators.keys())),
-                        self.point_mutate_feature(x),
+                        random.choice(
+                            [
+                                op
+                                for op, d in self.operators.items()
+                                if d.arity == self.operators[f].arity
+                            ]
+                        ),
+                        *map(self.point_mutate_feature, x),
                     )
-                return (f, self.point_mutate_feature(x))
+                return (f, *map(self.point_mutate_feature, x))
             case x:
                 if random.random() < p:
                     return random.choice(list(self.terminals.keys()))
@@ -182,20 +195,22 @@ class Evolver:
 
     def evolve_features(self, parameters: EvolutionParameters, fitness_f):
         population = [
-            self.random_feature(parameters.max_depth)
+            self.random_feature(parameters.max_depth, parameters.terminal_proba)
             for _ in range(parameters.pop_size)
         ]
         for g in range(parameters.n_generations):
             print(f"generation {g}")
             print("scoring features...")
             fitnesses = fitness_f(population)
-            print(f"max fitness = {fitnesses.max()}, mean fitness = {fitnesses.mean()}")
+            print(
+                f"max fitness={fitnesses.max():0.3}, mean fitness={fitnesses.mean():0.3}, best feature={self.simplify_feature(population[np.argmax(fitnesses)])}"
+            )
             if fitnesses.sum() == 0:
                 fitnesses += (
                     0.001  # random.choice doesn't like when the sum of weights is <= 0.
                 )
             new_population = []
-            for i in np.argsort(fitnesses)[-parameters.n_keep_best :]:
+            for i in reversed(np.argsort(fitnesses)[-parameters.n_keep_best :]):
                 new_population.append(population[i])
             while len(new_population) < parameters.pop_size:
                 if random.random() < parameters.crossover_rate:
@@ -240,6 +255,16 @@ class Evolver:
         print(f"mean score={scores.mean()}")
 
 
+# Operators.
+def safe_sum(x, y):
+    if len(y.shape) < len(x.shape):
+        x, y = y, x
+    # rank(x) <= rank(y)
+    while len(x.shape) < len(y.shape):
+        x = np.expand_dims(x, -1)
+    return x + y
+
+
 dataset = seisbench.data.WaveformDataset(
     Path.home() / ".seisbench/datasets/seismoslide_1"
 )
@@ -249,12 +274,12 @@ default_evolver = Evolver(
     operators={
         "min": OperatorDef(arity=1, delta_rank=-1, apply=lambda x: np.min(x, axis=-1)),
         "max": OperatorDef(arity=1, delta_rank=-1, apply=lambda x: np.max(x, axis=-1)),
-        "mean": OperatorDef(
-            arity=1, delta_rank=-1, apply=lambda x: np.mean(x, axis=-1)
-        ),
-        "median": OperatorDef(
-            arity=1, delta_rank=-1, apply=lambda x: np.median(x, axis=-1)
-        ),
+        # "mean": OperatorDef(
+        #     arity=1, delta_rank=-1, apply=lambda x: np.mean(x, axis=-1)
+        # ),
+        # "median": OperatorDef(
+        #     arity=1, delta_rank=-1, apply=lambda x: np.median(x, axis=-1)
+        # ),
         "skew": OperatorDef(
             arity=1, delta_rank=-1, apply=lambda x: scipy.stats.skew(x, axis=-1)
         ),
@@ -264,9 +289,10 @@ default_evolver = Evolver(
         "re_fft": OperatorDef(
             arity=1, delta_rank=0, apply=lambda x: np.real(scipy.fft.fft(x, axis=-1))
         ),
-        "im_fft": OperatorDef(
-            arity=1, delta_rank=0, apply=lambda x: np.imag(scipy.fft.fft(x, axis=-1))
-        ),
+        # "im_fft": OperatorDef(
+        #     arity=1, delta_rank=0, apply=lambda x: np.imag(scipy.fft.fft(x, axis=-1))
+        # ),
+        "+": OperatorDef(arity=2, delta_rank=0, apply=safe_sum),
     },
 )
 
@@ -274,32 +300,35 @@ default_evolver = Evolver(
 def benchmark_default_single():
     start_t = time.time()
     params = EvolutionParameters(
-        pop_size=15,
-        n_keep_best=3,
-        crossover_rate=0.7,
-        mutation_rate=0.1,
-        max_depth=5,
-        n_generations=10,
+        pop_size=40,
+        n_keep_best=10,
+        crossover_rate=0.4,
+        mutation_rate=0.4,
+        max_depth=7,
+        n_generations=30,
+        terminal_proba=0.5,
     )
     print(f"{params=}")
     fs = default_evolver.evolve_features(
         params, fitness_f=default_evolver.score_features
     )
-    print(f"best feature: {default_evolver.simplify_feature(fs[0])}")
+    best_feature = default_evolver.simplify_feature(fs[0])
+    print(f"best feature: {best_feature}")
     print(f"took {time.time() - start_t} s")
 
 
 def benchmark_default():
     start_t = time.time()
     params = EvolutionParameters(
-        pop_size=7,
-        n_keep_best=2,
-        crossover_rate=0.7,
-        mutation_rate=0.1,
-        max_depth=5,
-        n_generations=5,
+        pop_size=30,
+        n_keep_best=5,
+        crossover_rate=0.5,
+        mutation_rate=0.4,
+        max_depth=3,
+        n_generations=20,
+        terminal_proba=0.5,
     )
-    n_features = 5
+    n_features = 10
     print(f"{params=} {n_features=}")
     fs = default_evolver.meta_evolve_features(params, n_features)
     default_evolver.score_model(fs)
--- test_evolve.py
+++ test_evolve.py
@@ -4,7 +4,7 @@ import evolve
 def test_simplify_feature():
     # Simplifying a feature shouldn't change the results.
     for _ in range(10):
-        rf = evolve.default_evolver.random_feature(10)
+        rf = evolve.default_evolver.random_feature(10, 0.3)
         a = evolve.default_evolver.eval_feature(rf, simplify_first=True)
         b = evolve.default_evolver.eval_feature(rf, simplify_first=False)
         assert (a == b).all()
