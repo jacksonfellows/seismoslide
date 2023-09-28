117da7fdcb977336f1892b73cc1412dbc0def1f4
Jackson Fellows <jtfellows@icloud.com>
2023-09-28 17:20:44 -0400

    Some more plots..

---
+++ plot.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from evolve import default_evolver
+
+df = pd.read_csv("n_random_features_vs_model_score_cleanup.csv")
+
+
+def p():
+    plt.plot(df.n_random_features, df.model_score, "x")
+    plt.xlabel("# of Random Features (max_depth=5, terminal_proba=0.3)")
+    plt.ylabel("Model Performance (n_splits=8, test_size=0.25)")
+    plt.show()
+
+
+def plot_feature_importances(fs):
+    labels = [repr(default_evolver.simplify_feature(f)) for f in fs]
+    imps = default_evolver.calculate_permutation_importances(fs, n_jobs=-1)
+    x = range(len(labels))
+    C = plt.bar(x, imps.importances_mean, yerr=imps.importances_std)
+    plt.bar_label(C, labels)
+    plt.show()
