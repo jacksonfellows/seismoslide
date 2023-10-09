840f11e89a9a4cee8a926bc287cb5c98c04c284f
Jackson Fellows <jtfellows@icloud.com>
2023-10-09 12:25:56 -0400

    Cleanup.

--- evolve.py
+++ evolve.py
@@ -360,69 +360,68 @@ def make_bandpass_operator(freqmin, freqmax):
     return lambda x: bandpass(x, freqmin, freqmax, df=100, corners=2, zerophase=True)
 
 
+operators = {
+    "min": OperatorDef(
+        arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.min(x, axis=-1)
+    ),
+    "max": OperatorDef(
+        arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.max(x, axis=-1)
+    ),
+    "argmax": OperatorDef(
+        arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.argmax(x, axis=-1)
+    ),
+    "skew": OperatorDef(
+        arity=1,
+        delta_rank=-1,
+        cell_rank=1,
+        apply=lambda x: scipy.stats.skew(x, axis=-1),
+    ),
+    "kurtosis": OperatorDef(
+        arity=1,
+        delta_rank=-1,
+        cell_rank=1,
+        apply=lambda x: scipy.stats.kurtosis(x, axis=-1),
+    ),
+    "re_fft": OperatorDef(
+        arity=1,
+        delta_rank=0,
+        cell_rank=1,
+        apply=lambda x: np.real(scipy.fft.fft(x, axis=-1)),
+    ),
+    # "im_fft": OperatorDef(
+    #     arity=1, delta_rank=0, cell_rank=1, apply=lambda x: np.imag(scipy.fft.fft(x, axis=-1))
+    # ),
+    "+": OperatorDef(arity=2, delta_rank=0, cell_rank=0, apply=safe_add),
+    "corr": OperatorDef(arity=2, delta_rank=0, cell_rank=1, apply=safe_correlate),
+    "first_half": OperatorDef(arity=1, delta_rank=0, cell_rank=1, apply=first_half),
+    "second_half": OperatorDef(arity=1, delta_rank=0, cell_rank=1, apply=second_half),
+    "sum": OperatorDef(
+        arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.sum(x, axis=-1)
+    ),
+    "envelope": OperatorDef(arity=1, delta_rank=0, cell_rank=1, apply=envelope),
+    "periodogram": OperatorDef(arity=1, delta_rank=0, cell_rank=1, apply=periodogram),
+    "bp_1_3": OperatorDef(
+        arity=1, delta_rank=0, cell_rank=1, apply=make_bandpass_operator(1, 3)
+    ),
+    "bp_3_10": OperatorDef(
+        arity=1, delta_rank=0, cell_rank=1, apply=make_bandpass_operator(3, 10)
+    ),
+    "bp_10_20": OperatorDef(
+        arity=1, delta_rank=0, cell_rank=1, apply=make_bandpass_operator(10, 20)
+    ),
+    "bp_20_45": OperatorDef(
+        arity=1, delta_rank=0, cell_rank=1, apply=make_bandpass_operator(20, 45)
+    ),
+}
+
+
 dataset = seisbench.data.WaveformDataset(
     Path.home() / ".seisbench/datasets/seismoslide_1"
 )
 default_evolver = Evolver(
     W=dataset.get_waveforms(mask=np.ones(len(dataset), dtype=bool)),
     y=(dataset.metadata.source_type == "surface event").to_numpy(dtype=int),
-    operators={
-        "min": OperatorDef(
-            arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.min(x, axis=-1)
-        ),
-        "max": OperatorDef(
-            arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.max(x, axis=-1)
-        ),
-        "argmax": OperatorDef(
-            arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.argmax(x, axis=-1)
-        ),
-        "skew": OperatorDef(
-            arity=1,
-            delta_rank=-1,
-            cell_rank=1,
-            apply=lambda x: scipy.stats.skew(x, axis=-1),
-        ),
-        "kurtosis": OperatorDef(
-            arity=1,
-            delta_rank=-1,
-            cell_rank=1,
-            apply=lambda x: scipy.stats.kurtosis(x, axis=-1),
-        ),
-        "re_fft": OperatorDef(
-            arity=1,
-            delta_rank=0,
-            cell_rank=1,
-            apply=lambda x: np.real(scipy.fft.fft(x, axis=-1)),
-        ),
-        # "im_fft": OperatorDef(
-        #     arity=1, delta_rank=0, cell_rank=1, apply=lambda x: np.imag(scipy.fft.fft(x, axis=-1))
-        # ),
-        "+": OperatorDef(arity=2, delta_rank=0, cell_rank=0, apply=safe_add),
-        "corr": OperatorDef(arity=2, delta_rank=0, cell_rank=1, apply=safe_correlate),
-        "first_half": OperatorDef(arity=1, delta_rank=0, cell_rank=1, apply=first_half),
-        "second_half": OperatorDef(
-            arity=1, delta_rank=0, cell_rank=1, apply=second_half
-        ),
-        "sum": OperatorDef(
-            arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.sum(x, axis=-1)
-        ),
-        "envelope": OperatorDef(arity=1, delta_rank=0, cell_rank=1, apply=envelope),
-        "periodogram": OperatorDef(
-            arity=1, delta_rank=0, cell_rank=1, apply=periodogram
-        ),
-        "bp_1_3": OperatorDef(
-            arity=1, delta_rank=0, cell_rank=1, apply=make_bandpass_operator(1, 3)
-        ),
-        "bp_3_10": OperatorDef(
-            arity=1, delta_rank=0, cell_rank=1, apply=make_bandpass_operator(3, 10)
-        ),
-        "bp_10_20": OperatorDef(
-            arity=1, delta_rank=0, cell_rank=1, apply=make_bandpass_operator(10, 20)
-        ),
-        "bp_20_45": OperatorDef(
-            arity=1, delta_rank=0, cell_rank=1, apply=make_bandpass_operator(20, 45)
-        ),
-    },
+    operators=operators,
 )
 
 
