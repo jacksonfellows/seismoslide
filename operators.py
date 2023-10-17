ea6d7bf79f0bf6a88989495715817236c30ba01e
Jackson Fellows <jtfellows@icloud.com>
2023-10-17 13:12:23 -0400

    More operators. Also make_same_dims.

--- operators.py
+++ operators.py
@@ -8,30 +8,30 @@ from obspy.signal.filter import bandpass
 OperatorDef = namedtuple("OperatorDef", ("arity", "delta_rank", "apply", "cell_rank"))
 
 
-def make_same_rank(f):
+def make_same_dims(f):
     @wraps(f)
-    def wrapper(*args):
-        max_rank = max(len(x.shape) for x in args)
-        new_args = []
-        for arg in args:
-            while len(arg.shape) < max_rank:
-                arg = np.expand_dims(arg, -1)
-            new_args.append(arg)
-        return f(*new_args)
+    def wrapper(x, y):
+        max_rank = max(len(x.shape), len(y.shape))
+        # Make same rank.
+        while len(x.shape) < max_rank:
+            x = np.expand_dims(x, -1)
+        while len(y.shape) < max_rank:
+            y = np.expand_dims(y, -1)
+        # Make same dims.
+        min_dims = tuple(min(a, b) for a, b in zip(x.shape, y.shape))
+        x = x[tuple(slice(0, i) for i in min_dims)]
+        y = y[tuple(slice(0, i) for i in min_dims)]
+        return f(x, y)
 
     return wrapper
 
 
-@make_same_rank
+@make_same_dims
 def safe_add(x, y):
-    if x.shape[-1] < y.shape[-1]:
-        y = y[..., : x.shape[-1]]
-    elif y.shape[-1] < x.shape[-1]:
-        x = x[..., : y.shape[-1]]
     return x + y
 
 
-@make_same_rank
+@make_same_dims
 def safe_correlate(x, y):
     res = np.empty_like(x)
     for i in np.ndindex(res.shape[:-1]):
@@ -68,8 +68,17 @@ all_operators = {
     "max": OperatorDef(
         arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.max(x, axis=-1)
     ),
+    "mean": OperatorDef(
+        arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.mean(x, axis=-1)
+    ),
+    "median": OperatorDef(
+        arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.median(x, axis=-1)
+    ),
     "argmax": OperatorDef(
         arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.argmax(x, axis=-1)
+    ),
+    "argmin": OperatorDef(
+        arity=1, delta_rank=-1, cell_rank=1, apply=lambda x: np.argmin(x, axis=-1)
     ),
     "skew": OperatorDef(
         arity=1,
@@ -83,6 +92,12 @@ all_operators = {
         cell_rank=1,
         apply=lambda x: scipy.stats.kurtosis(x, axis=-1),
     ),
+    "len": OperatorDef(
+        arity=1,
+        delta_rank=-1,
+        cell_rank=1,
+        apply=lambda x: np.full(x.shape[:-1], x.shape[-1]),
+    ),
     "re_fft": OperatorDef(
         arity=1,
         delta_rank=0,
@@ -90,7 +105,10 @@ all_operators = {
         apply=lambda x: np.real(scipy.fft.fft(x, axis=-1)),
     ),
     # "im_fft": OperatorDef(
-    #     arity=1, delta_rank=0, cell_rank=1, apply=lambda x: np.imag(scipy.fft.fft(x, axis=-1))
+    #     arity=1,
+    #     delta_rank=0,
+    #     cell_rank=1,
+    #     apply=lambda x: np.imag(scipy.fft.fft(x, axis=-1)),
     # ),
     "+": OperatorDef(arity=2, delta_rank=0, cell_rank=0, apply=safe_add),
     "corr": OperatorDef(arity=2, delta_rank=0, cell_rank=1, apply=safe_correlate),
@@ -112,5 +130,19 @@ all_operators = {
     ),
     "bp_20_45": OperatorDef(
         arity=1, delta_rank=0, cell_rank=1, apply=make_bandpass_operator(20, 45)
+    ),
+    "num_peaks": OperatorDef(
+        arity=1,
+        delta_rank=-1,
+        cell_rank=1,
+        apply=lambda x: np.apply_along_axis(
+            lambda y: scipy.signal.find_peaks(y)[0].shape[0], -1, x
+        ),
+    ),
+    "spectrogram": OperatorDef(
+        arity=1,
+        delta_rank=+1,
+        cell_rank=1,
+        apply=lambda x: scipy.signal.spectrogram(x, axis=-1)[2],
     ),
 }
