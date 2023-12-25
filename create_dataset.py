4770f90dd255676f054ef8df5ce0118120c65529
Jackson Fellows <jtfellows@icloud.com>
2023-12-25 12:33:09 -0800

    Add explosions and noise waveforms.

--- create_dataset.py
+++ create_dataset.py
@@ -56,30 +56,35 @@ def qualified_station_name(metadata):
 
 
 pnw_exotic = seisbench.data.PNWExotic()
+pnw_noise = seisbench.data.PNWNoise()
 pnw = seisbench.data.PNW()
 
 np_gen = np.random.default_rng(123)
 
 
 def save_event(waveform, metadata, writer, sampling_rate):
-    trace_P_arrival_sample = metadata.trace_P_arrival_sample
-    if np.isnan(trace_P_arrival_sample):
-        print("skipping event - no trace_P_arrival_sample")
-        return
-    shifted, shift_samples = shift_P_arrival(
-        waveform, trace_P_arrival_sample, sampling_rate=sampling_rate
-    )
-    normalized = normalize(shifted)
+    if metadata.source_type != "noise":
+        trace_P_arrival_sample = metadata.trace_P_arrival_sample
+        if np.isnan(trace_P_arrival_sample):
+            print("skipping event - no trace_P_arrival_sample")
+            return
+        shifted, shift_samples = shift_P_arrival(
+            waveform, trace_P_arrival_sample, sampling_rate=sampling_rate
+        )
+        normalized = normalize(shifted)
+        trace_start_time = (
+            UTCDateTime(metadata.trace_start_time) + shift_samples / sampling_rate
+        )
+    else:
+        normalized = normalize(waveform[:, :6000])
+        trace_start_time = UTCDateTime(metadata.trace_start_time)
     if normalized.shape != (3, 6000):
         print(f"skipping event - malformed shape {normalized.shape}")
         return
-    trace_start_time = (
-        UTCDateTime(metadata.trace_start_time) + shift_samples / sampling_rate
-    )
     writer.add_trace(
         {
             "source_type": metadata.source_type,
-            "event_id": metadata.event_id,
+            "event_id": metadata.get("event_id"),
             "station_network_code": metadata.station_network_code,
             "station_code": metadata.station_code,
             "station_location_code": metadata.station_location_code,
@@ -89,7 +94,7 @@ def save_event(waveform, metadata, writer, sampling_rate):
     )
 
 
-def write_split(i_eq, i_su, base_path):
+def write_split(i_su, i_eq, i_ex, i_noise, base_path):
     metadata_path = base_path / "metadata.csv"
     waveforms_path = base_path / "waveforms.hdf5"
     with seisbench.data.WaveformDataWriter(metadata_path, waveforms_path) as writer:
@@ -99,7 +104,9 @@ def write_split(i_eq, i_su, base_path):
             "component_order": "ZNE",
             "sampling_rate": sampling_rate,
         }
-        for indices, dataset in zip([i_eq, i_su], [pnw, pnw_exotic]):
+        for indices, dataset in zip(
+            [i_su, i_eq, i_ex, i_noise], [pnw_exotic, pnw, pnw, pnw_noise]
+        ):
             for i in indices:
                 save_event(
                     dataset.get_waveforms(i),
@@ -112,39 +119,38 @@ def write_split(i_eq, i_su, base_path):
 def make_splits(split_dir):
     all_su = pnw_exotic.metadata[pnw_exotic.metadata.source_type == "surface event"]
     all_eq = pnw.metadata[pnw.metadata.source_type == "earthquake"]
-
-    q_eq = qualified_station_name(all_eq)
-    q_su = qualified_station_name(all_su)
-
-    # Awful.
-    counts_eq = q_eq.to_frame().join(
-        q_su.value_counts().reindex(q_eq.unique(), fill_value=0), on=0
-    )["count"]
-
-    p = counts_eq.to_numpy()
-    p = p / p.sum()
-
-    # Earthquake indices: Random choice weighed by # of surface events/station.
-    i_eq = np_gen.choice(all_eq.index.to_numpy(), size=len(all_su), replace=False, p=p)
+    all_ex = pnw.metadata[pnw.metadata.source_type == "explosion"]
+    all_noise = pnw_noise.metadata
 
     # Surface event indices: Random permutation of all events.
     i_su = np_gen.permutation(all_su.index.to_numpy())
 
-    assert i_eq.shape == i_su.shape
+    # Take as many earthquakes, explosions, and noise as there are surface events.
+    i_eq = np_gen.choice(all_eq.index.to_numpy(), size=len(i_su))
+    i_ex = np_gen.choice(all_ex.index.to_numpy(), size=len(i_su))
+    i_noise = np_gen.choice(all_noise.index.to_numpy(), size=len(i_su))
+
+    assert i_su.shape == i_eq.shape == i_ex.shape == i_noise.shape
 
     # Split percentages:
     train_p, valid_p, test_p = 0.6, 0.3, 0.1
     assert train_p * 100 + valid_p * 100 + test_p * 100 == 100
-    train_i_eq, valid_i_eq, test_i_eq = np.array_split(
-        i_eq, [int(train_p * len(i_eq)), int((train_p + valid_p) * len(i_eq))]
-    )
     train_i_su, valid_i_su, test_i_su = np.array_split(
         i_su, [int(train_p * len(i_su)), int((train_p + valid_p) * len(i_su))]
     )
+    train_i_eq, valid_i_eq, test_i_eq = np.array_split(
+        i_eq, [int(train_p * len(i_eq)), int((train_p + valid_p) * len(i_eq))]
+    )
+    train_i_ex, valid_i_ex, test_i_ex = np.array_split(
+        i_ex, [int(train_p * len(i_ex)), int((train_p + valid_p) * len(i_ex))]
+    )
+    train_i_noise, valid_i_noise, test_i_noise = np.array_split(
+        i_noise, [int(train_p * len(i_noise)), int((train_p + valid_p) * len(i_noise))]
+    )
 
-    write_split(train_i_eq, train_i_su, split_dir / "train")
-    write_split(valid_i_eq, valid_i_su, split_dir / "valid")
-    write_split(test_i_eq, test_i_su, split_dir / "test")
+    write_split(train_i_su, train_i_eq, train_i_ex, train_i_noise, split_dir / "train")
+    write_split(valid_i_su, valid_i_eq, valid_i_ex, valid_i_noise, split_dir / "valid")
+    write_split(test_i_su, test_i_eq, test_i_ex, test_i_noise, split_dir / "test")
 
 
 def create_splits():
