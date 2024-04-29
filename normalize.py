4c0ff223235189af3de3bc9547955bf8a910a229
Jackson Fellows <jtfellows@icloud.com>
2024-04-29 08:29:37 -0400

    Update normalize (tested first!)

--- normalize.py
+++ normalize.py
@@ -2,19 +2,7 @@ import numpy as np
 import scipy
 
 
-def taper(waveform, taper_len_s, freq):
-    taper_len = taper_len_s * freq
-    hann = scipy.signal.windows.hann(2 * taper_len)
-    taper = np.ones(waveform.shape[-1])
-    taper[0:taper_len] = hann[0:taper_len]
-    taper[-taper_len:] = hann[-taper_len:]
-    return taper * waveform
-
-
 def normalize(waveform):
     """Normalize a waveform for testing/prediction."""
-    normalized = scipy.signal.detrend(waveform)  # Should I also remove mean?
-    stddev = np.std(normalized)
-    if stddev != 0:
-        normalized /= np.std(normalized)  # std vs max?
-    return normalized
+    normalized = scipy.signal.detrend(waveform, axis=-1, type="constant")
+    return normalized / np.std(normalized, axis=-1)[:, None]
